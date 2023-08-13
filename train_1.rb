2d84a92844af682f25e271782105714509b6c41d
Zacbouu <zacharia_ha@yahoo.fr>
2023-08-13 16:20:46 +0200

    training solo

---
+++ train_1.rb
@@ -0,0 +1,22 @@
+puts "Salut, Se7en"
+print ">"
+user_name = gets.chomp
+puts "je te connais, je sais ou tu vis, qui tu es, je sais tout de toi"
+print ">"
+answer = gets.chomp
+puts "je te surveille depuis très longtemps"
+print ">"
+answer = gets.chomp
+puts "hahaha je te vois"
+print ">"
+answer = gets.chomp
+puts "regarde par la fenêtre"
+print ">"
+answer = gets.chomp
+puts "choisis un chiffre humain que tu es..."
+print ">"
+numbers = gets.chomp.to_i
+numbers.times do
+  puts "you will die hahaha"
+end
+
